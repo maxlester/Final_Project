1dd885184e7b23a7bc69b9b8f6c2112895f984d7
Justinette2175 <jugagnepain75@gmail.com>
2017-02-23 18:15:30 +0000

    capitalized App.js

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Welcome to our routed page</h1>
+        <p>We hope you have great time.</p>
+      </div>
+    );
+  }
+}
