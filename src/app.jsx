b6063370c4629d8ac036642cf2e4fb07fe22d73c
Max Lester <maxlester18@gmail.com>
2017-02-23 12:52:39 -0500

    ready to start

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <h1>Hello World!</h1>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
