caf35526e6edb0088f5b747244bff249bdcf4b95
Max Lester <maxlester18@gmail.com>
2017-02-24 15:41:16 -0500

    implemented server + knex

--- knexfile.js
+++ knexfile.js
@@ -6,11 +6,8 @@ module.exports = {
     client: 'postgresql',
     connection: {
       host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
       database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      port     : process.env.DB_PORT
     },
     migrations: {
       directory: './db/migrations',
--- web_server/server.js
+++ web_server/server.js
@@ -1,31 +1,32 @@
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var knexConfig  = require("../knexfile");
-var knex = require('knex');
-var PORT = process.env.PORT || 8080;
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cookieSession = require('cookie-session')
+const bcrypt = require('bcrypt');
 
-var cookieSession = require('cookie-session')
-var bcrypt = require('bcrypt');
+const knexConfig  = require("../knexfile");
+const knex = require('knex')(knexConfig[ENV]);
+const knexLogger  = require('knex-logger');
+const morgan      = require('morgan');
+
 
 app.use(morgan('dev'));
-
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
-
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('public'));
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2'],
 }))
 
 app.get('/', function(req, res) {
-  let currentUser = knex('users').where('first_name', "Alice");
-  res.json({currentUser : currentUser});
+  knex('users').where('first_name', "Alice").then((result) => {
+    console.log(result);
+    res.json({currentUser : result})
+  })
 });
 
 app.listen(PORT, function(){
