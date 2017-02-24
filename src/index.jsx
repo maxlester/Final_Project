ba3feccade7eaf5c3800ce134550f6eecdc7185e
Max Lester <maxlester18@gmail.com>
2017-02-24 13:40:42 -0500

    rendered dashboard page

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,16 @@ require("../styles/application.scss");
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, hashHistory, IndexRoute} from 'react-router';
 import App from './app.jsx';
+import HomePage from './home.jsx';
+import Dashboard from './dashboard.jsx';
 
-
-ReactDOM.render(<App />, document.getElementById('react-root'));
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={HomePage} />
+      <Route path="dashboard" component={Dashboard}/>
+    </Route>
+  </Router>
+),document.getElementById('react-root'));
