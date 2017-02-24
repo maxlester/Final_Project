52bc5a6a76a3954f088c015353fe99bc19aafc0f
Justinette2175 <jugagnepain75@gmail.com>
2017-02-24 17:08:14 -0500

    implementing ajax request for form

--- src/Home/RegisterStudent.jsx
+++ src/Home/RegisterStudent.jsx
@@ -8,12 +8,12 @@ class RegisterStudent extends Component {
   render() {
     return(
        <form className="registerStudent">
-        <input className="registerStudent-firstName" type= "text" placeholder="First Name"/>
-        <input className="registerStudent-lastName" type= "text" placeholder="Last Name"/>
-        <input className="registerStudent-username" type= "text" placeholder="User Name"/>
-        <input className="registerStudent-email" type= "text" placeholder="Email"/>
-        <input className="registerStudent-password" type= "text" placeholder="Password"/>
-        <button type="submit">Submit</button>
+        <input className="firstName" type= "text" placeholder="First Name" onChange={this.handleEmailChange}/>
+        <input className="lastName" type= "text" placeholder="Last Name" onChange={this.handleEmailChange}/>
+        <input className="username" type= "text" placeholder="User Name" onChange={this.handleEmailChange}/>
+        <input className="email" type= "email" placeholder="Email" onChange={this.handleEmailChange}/>
+        <input className="password" type= "text" placeholder="Password" onChange={this.handleEmailChange}/>
+        <button type="submit" onClick={this.props.registerUser}>Submit</button>
       </form>
     )
   }
--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 import NavBar from '../navBar.jsx';
-import RegisterStudent from './RegisterStudent.jsx'
-import RegisterTeacher from './RegisterTeacher.jsx'
+import RegisterStudent from './RegisterStudent.jsx';
+import RegisterTeacher from './RegisterTeacher.jsx';
 
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
   }
+
+  registerUser(e){
+    let form = e.target;
+    let firstName = form.querySelector('.firstName').value();
+    console.log(firstName);
+  }
+
   render() {
     return (
       <div className="home">
@@ -19,8 +26,8 @@ class HomePage extends Component {
           <section className="register">
             <button type="button" className="btn btn-default">I want to teach</button>
             <button type="button" className="btn btn-clear">I want to take classes</button>
-            <RegisterStudent/>
-            <RegisterTeacher/>
+            <RegisterStudent registerUser = {this.registerUser.bind(this)}/>
+            // <RegisterTeacher/>
           </section>
         </main>
       </div>
