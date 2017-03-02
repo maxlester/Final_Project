d55cc9b2df8e22577038d8ac571e5192d3aa1b4d
Justinette2175 <jugagnepain75@gmail.com>
2017-03-02 14:44:29 -0500

    made query to get class information

--- src/Conference/conference.jsx
+++ src/Conference/conference.jsx
@@ -7,8 +7,10 @@ import ConferenceMain from './conferenceMain.jsx';
 class Conference extends Component {
   constructor(props) {
     super(props);
+    let classId = this.props.params.classId;
     this.state = {
-      startConference : false
+      startConference : false,
+      classId : classId
     }
   }
 
@@ -17,14 +19,32 @@ class Conference extends Component {
     this.setState({startConference : true})
   }
 
+  getConferenceData(){
+    let classId = this.state.classId;
+    let authUserId = Auth.retrieveUser().userId
+    $.ajax({
+      url: `http://localhost:8080/class/${classId}`,
+      type: 'GET',
+      context: this,
+      success: function(data) {
+        // let user = JSON.parse(data);
+        this.setClassesTaking(data)
+      },
+      error: function(xhr, status, err) {
+        console.error(err.toString());
+      }.bind(this)
+   })
+   return false; //returning false to prevent info showing in url
+  }
+
   render() {
     if (this.state.startConference){
       return (
-        <ConferenceMain router={this.props.router} classId = {this.props.params.classId}/>
+        <ConferenceMain router={this.props.router} classId = {this.state.classId}/>
       );
     }
     else {
-      return (<PreConference startConference={this.startConference.bind(this)} classId = {this.props.params.classId}/>)
+      return (<PreConference startConference={this.startConference.bind(this)} classId = {this.state.classId}/>)
     }
   }
 }
