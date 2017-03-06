29e91b3185f358497b0476c7dccdd2c2999eeb1d
Justinette2175 <jugagnepain75@gmail.com>
2017-03-06 14:50:25 -0500

    styled teacher profile classes

--- src/TeacherProfile/teacherClassList.jsx
+++ src/TeacherProfile/teacherClassList.jsx
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
 import TeacherClass from './teacherClass.jsx';
+var moment = require('moment');
+
 
 
 class TeacherClassList extends Component {
 
+  formatDate(date){
+    let array = moment(date).toArray();
+    let string = moment(date).format('MMMM Do YYYY, h:mm a');
+    let formatted = [];
+    formatted[0] = array[0];
+    formatted[2] = array[2];
+    formatted[1]= moment(formatted[1]).format('MMMM');
+    formatted[3]= string.substr(string.indexOf(",") + 1);
+    return formatted;
+  }
+
   render() {
     var teacherClasses = this.props.teacherClasses.map((item)=>{
       console.log("ITEM", item)
-      return <TeacherClass key = {item.id} teacherId = {this.props.teacherId} classTitle = {item.classTitle} classDate = {item.classDate} classDescription = {item.classDescription} classCost={item.classCost} id={item.classId} router={this.props.router} maxNumberOfStudents = {item.maxNumberOfStudents} numberOfStudents = {item.numberOfStudents} router = {this.props.router}/>
+      return <TeacherClass key = {item.id} formatDate = {this.formatDate.bind(this)} teacherId = {this.props.teacherId} classTitle = {item.classTitle} classDate = {item.classDate} classDescription = {item.classDescription} classCost={item.classCost} id={item.classId} router={this.props.router} maxNumberOfStudents = {item.maxNumberOfStudents} numberOfStudents = {item.numberOfStudents} router = {this.props.router}/>
     })
     return (
       <div>
